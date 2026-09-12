3a74921f8e57eff94d8b8b59666892ec09949977
sebastian oliveto <s.oliveto93@gmail.com>
2026-09-11 22:27:28 -0300

    se agrega readme guia de ejecucion en fase desarrollo

--- rest/src/main/java/com/example/rest/controller/UsuarioController.java
+++ rest/src/main/java/com/example/rest/controller/UsuarioController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.rest.dto.UsuarioResponseDTO;
-import com.example.rest.entity.Usuario;
 import com.example.rest.repository.UsuarioRepository;
 
 @RestController
